9a2e5aa024fa736703be17681ab50a59b80cfae5
Bhargava-123 <bhargavashankar2003@gmail.com>
2023-09-16 14:42:05 +0530

    python notebook

--- demo.py
+++ demo.py
@@ -6,39 +6,54 @@ def get_herbs_name_dict():
     
     # print(len(reader.pages))
     herbs_name_dict = dict()
+    count  = 1
 
     for i in range(len(reader.pages)):
+        # print(count)
+        count += 1
+
         page = reader.pages[i]
         listy = page.extract_text().split("\n")
         num = listy[0].split()
-        # print(num)
-        try:
-            if(num[0][0].isdigit()):
-                herb_name = ""
-                for ele in num[1:] :  herb_name += ele
-                herbs_name_dict[num[0][:-1]] = {"Name" : herb_name}
+        Rasa = ""
+        Guna = ""
+        Vipaka = ""
+        Virya = ""
+        temp_num = ""
 
-                # for line in listy:
-                #     # if "Rasa" in line or "Guna" in line or"Virya" in line or "Vipaka" in line or "Karma" in line:'
-                #     if "Rasa" in line:
-                #         print(line)
-                #         x = re.split(":",line)
-                #         rasa_list = [ele.strip() for ele in x]
-                #         herbs_name_dict[num[0][:-1]]["Rasa"] = ele[1]
-                        
-                # herbs_name_dict[num[0][:-1]] = {"Name" : herb_name,}
-        except:
-            pass
-        for line in listy:
-                    # if "Rasa" in line or "Guna" in line or"Virya" in line or "Vipaka" in line or "Karma" in line:'
-            if "Rasa" in line:
-                print(line)
-                x = re.split(":",line)
-                rasa_list = [ele.strip() for ele in x]
-                herbs_name_dict[num[0][:-1]]["Rasa"] = ele[1]
+        print(listy[0])
+        
+        
+        # print(listy[0])
+        # if(num[0][0].isdigit()):
+        #     herb_name = ""
+        #     for ele in num[1:] :  herb_name += ele
+        #     herbs_name_dict[num[0][:-1]] = {"Name" : herb_name}
         # for line in listy:
-        #     if "Rasa" in line or "Guna" in line or"Virya" in line or "Vipaka" in line or "Karma" in line:
-        #         print(line)
+        #     if "Rasa" in line:
+        #         x = re.split(':',line)
+        #         rasa_list = [ele.strip() for ele in x]
+        #         for ele in rasa_list:
+        #             Rasa += ele + ","
+        #         # print("Rasa->",Rasa)
+        #     if "Guna" in line:
+        #         x = re.split(':',line)
+        #         guna_list = [ele.strip() for ele in x]
+        #         for ele in guna_list:
+        #             Guna += ele + ","
+        #         # print("Guna->",Guna)
+        #     if "Virya" in line:
+        #         x = re.split(':',line)
+        #         virya_list = [ele.strip() for ele in x]
+        #         for ele in virya_list:
+        #             Virya += ele + ","
+        #         # print("Virya->",Virya)
+        #     if "Vipaka" in line:
+        #         x = re.split(':',line)
+        #         vipaka_list = [ele.strip() for ele in x]
+        #         for ele in vipaka_list:
+        #             Vipaka += ele + ","
+        #         # print("Vipaka->",Vipaka)
 
     print(herbs_name_dict)
     return herbs_name_dict
